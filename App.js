867032887201603550042a928d57738bfb438ac2
KSwitch1303 <ebimieteimisongo@gmail.com>
2023-08-15 22:09:44 +0100

    master commit

---
+++ App.js
@@ -0,0 +1,11 @@
+let x = 'Favour';
+console.log('First string method ' + x);
+console.log("Second string method " + x + " asdkasjh");
+console.log(`Third string method ${x} sahdkjasdihi`)
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/KSwitch1303/First_class.git
+git push -u origin main
